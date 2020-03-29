844249a226219dca7420fe9c0c018191caec76a9
Thomas Tacke <206163-RootTheKid@users.noreply.gitlab.com>
2020-03-29 07:14:59 +0200

    First version of types

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,5 @@
 import { Observable } from 'rxjs';
 
-export const enum ICONS {
-  TEMPERATURE = 'thermometer',
-  HUMIDITY = 'water-percent',
-  LIGHTBULB_ON = 'lightbulb-on',
-  LIGHTBULB_OFF = 'lightbulb'
-}
-
 export const enum DEVICE {
   TADO = 'Tado',
   NODEMCU = 'NodeMCU',
@@ -15,9 +8,9 @@ export const enum DEVICE {
 
 export interface Sensor<T = any> {
   Name: string;
-  Value?: Observable<T>;
   Topic: string;
   Device: DEVICE;
+  Value?: Observable<T>;
 }
 
 export interface TemperatureSensor extends Sensor<number> {
@@ -28,19 +21,7 @@ export interface LightSensor extends Sensor<string> {
   Value: Observable<string>;
 }
 
-export interface UiSensor<T = any> extends Sensor<T> {
-  SvgIcon?: string;
-}
-
-export type UiTemperatureSensor = UiSensor & TemperatureSensor;
-
 export interface Room {
   Name: string;
-  Sensors: Sensor[];
+  Sensors: Array<Sensor>;
 }
-
-// () => {
-//   const data: UiTemperatureSensor = {
-//     Value: 3
-//   };
-// }();
